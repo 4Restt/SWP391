12994cdcfcbd496f1d204b1774b6d4b04e30e1de
LamVNT <lam2482003lc@gmail.com>
2024-01-30 16:49:25 +0700

    Update images and database

--- SWP391-Project-Zara/src/java/Controllers/Search.java
+++ SWP391-Project-Zara/src/java/Controllers/Search.java
@@ -28,18 +28,26 @@ public class Search extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         search.add("name");
-        search.add("descrisption");
-        search.add("price");
-        search.add("size");
+        search.add("description");
         search.add("color");
+        search.add("size");
         String txt = request.getParameter("search");
-        String sql = "select * from Product where ";
+        String sql = "WITH RankedProducts AS (\n"
+                + "	 SELECT P.id, P.product_info_id, P.size,  P.color, P.name, P.quantity, P.description, PI.price, PI.img_default,\n"
+                + "	ROW_NUMBER() OVER (PARTITION BY P.product_info_id ORDER BY P.id) AS rn\n"
+                + "	FROM  Product P\n"
+                + "	JOIN ProductInfor PI ON P.product_info_id = PI.id\n"
+                + "	JOIN Style S ON PI.style_id = S.id\n"
+                + "	JOIN Category C ON S.cate_id = C.id\n"
+                + ")\n"
+                + "SELECT id, product_info_id, size, color, name, quantity, description, price, img_default FROM RankedProducts WHERE rn = 1 and (";
         String s1 = "";
         for (String s2 : search) {
             s1 += "[" + s2 + "] like CONCAT ('%',?,'%') or ";
         }
         s1 = s1.substring(0, s1.length() - 4);
         sql += s1;
+        sql += ")";
         List<Category> cat = CategoryDAO.INSTANCE.getListCategory();
         request.setAttribute("cat", cat);
         ProductDAO.INSTANCE.Search(sql, txt, search.size());
@@ -52,43 +60,20 @@ public class Search extends HttpServlet {
             request.setAttribute("txt", txt);
             request.getRequestDispatcher("Views/Shop.jsp").forward(request, response);
         }
-
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
